33f5365fc597ee96997ccd477b7b5b69f2447e86
Stephen Beck <beck@j38.uk>
2026-04-30 22:23:59 -0400

    nix update

--- nixos/home/flake.nix
+++ nixos/home/flake.nix
@@ -25,7 +25,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, nix-index-database, home-manager, zen-browser, nixpkgs-stable, noctalia, ... }@inputs:
