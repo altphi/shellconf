9cbe5975fbe07808932d27f85402f1ca3b42b1f1
Stephen Beck <beck@j38.uk>
2026-05-30 22:29:03 -0400

    nix updates

--- nixos/home/nixvim/01-core.nix
+++ nixos/home/nixvim/01-core.nix
@@ -10,8 +10,8 @@
     };
 
     opts = {
-      number = false;
-      relativenumber = false;
+      number = true;
+      relativenumber = true;
       tabstop = 2;
       shiftwidth = 2;
       expandtab = true;
