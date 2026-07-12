f632842e4186f1a93980fbdba92f5da302cebcdf
Stephen Beck <beck@j38.uk>
2026-07-11 20:16:51 -0400

    clean up

--- niri-focus-or-open-tmux.sh
+++ niri-focus-or-open-tmux.sh
@@ -7,17 +7,14 @@ else
   NAME="com.tmux.main"
 fi
 
-WINDOW_ENTRY=`niri msg -j windows | jq -r '.[] | "\(.app_id),\(.id)"' | grep "${NAME}"`
+WINDOW_ENTRY=$(niri msg -j windows | jq -r '.[] | "\(.app_id),\(.id)"' | grep "${NAME}")
 
 if [[ ! -z "$WINDOW_ENTRY" ]]; then
   echo "$WINDOW_ENTRY" | head -n 1 | cut -d, -f2 | xargs niri msg action focus-window --id
 else
-  # exec footclient --app-id ${NAME} zsh
   if tmux has-session 2>/dev/null; then
-    exec ghostty --class=${NAME} -e tmux attach
-    # exec footclient --app-id ${NAME} --title=${NAME} tmux attach
+    exec ghostty --class="${NAME}" -e tmux attach
   else
-    exec ghostty --class=${NAME} -e zsh
-    # exec footclient --app-id ${NAME} --title=${NAME} zsh
+    exec ghostty --class="${NAME}" -e zsh
   fi
 fi
